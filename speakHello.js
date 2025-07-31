168ffb28b51bc31ca8a7531f6e6fa344bd8627a2
Arslan Zulfiqar <129317353+arslanzu@users.noreply.github.com>
2025-07-31 22:34:39 +0500

    Add files via upload

---
+++ speakHello.js
@@ -0,0 +1,7 @@
+// STEP 1: Create helloSpeaker
+var helloSpeaker = {};
+
+// STEP 2: Attach speak function to helloSpeaker
+helloSpeaker.speak = function(name) {
+  console.log("Hello " + name);
+};
